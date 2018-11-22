76972fd8b8b38cb56981eb875a4a5f838233f4d3
Simon Persson <simonpersson@flaskpost.org>
2018-11-22 16:56:07 +0100

    Started working on typechecker

--- Lab2/java/TypeChecker.java
+++ Lab2/java/TypeChecker.java
@@ -1,26 +1,53 @@
 import CPP.Absyn.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Iterator;
+import java.util.Set;
 
 public class TypeChecker {
-    public interface Visitor <R,A> {
-      public R visit(CPP.Absyn.Program p, A arg);
+        public class Sigma{
+            private AType[] arguments;
+            private AType returnType;
+            public AType[] getArgs(){
+                return arguments;
+            }
+            public AType getReturnType(){
+                return returnType;
+            }
+            public Sigma(ListArg args, Type type){
+                arguments = new AType[args.length];
+                for (int i =0; i<args.length; i++){
+                    arguments[i] = setType(args[i]);
+                }
+                returnType=setType(type);
+            }
 
-    }
-
-    public void typecheck(Program p) {
-        PDefs test = (PDefs)p;
-        ListDef hej = test.listdef_;
-
-        // p.accept(Visitor <R,A>);
-        // PDefs test = p.accept();
-        // ListDef test2 = test.listdef_;
-        for (Def def : hej) {
-            System.out.println("Död");
-            System.out.println(hej.size());
         }
-        // for (int i =0;i<p.length() ; i++ ) {
-        //     System.out.println("PLT SUGER");
-        //
-        // }
+        public enum AType{INT,DOUBLE, BOOL, VOID; };
+        public HashMap<String,Sigma> symbolTable = new HashMap<String,Sigma>();
+
+        public AType setType(Type type){
+            if (type instanceof Type_int)  return AType.INT;
+            else if(type instanceof Type_double) return AType.DOUBLE;
+            else if(type instanceof Type_bool) return AType.BOOL;
+            else if(type instanceof Type_void) return AType.VOID;
+            else throw new TypeException("Not yet a typechecker");
+
+            }
+
+
+        public void typecheck(Program p) {
+
+        PDefs defs = (PDefs)p;
+        ListDef listOfDefs = defs.listdef_;
+
+        for (Def def : listOfDefs) {
+            DFun function = (DFun)def;
+
+            symbolTable.put(function.id_, new Sigma(function.listarg_, function.type_));
+
+        }
+
         throw new TypeException("Not yet a typechecker");
     }
 
