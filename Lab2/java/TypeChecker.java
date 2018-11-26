108920958b39a6585a852e3111e6e50757ed9d91
Karl <karlstrigen@gmail.com>
2018-11-26 15:53:35 +0100

    lite kod fran slides

--- Lab2/java/TypeChecker.java
+++ Lab2/java/TypeChecker.java
@@ -1,53 +1,75 @@
 import CPP.Absyn.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Iterator;
-import java.util.Set;
-import CPP.PrettyPrinter;
+import java.util.*;
+//import CPP.PrettyPrinter;
 
 public class TypeChecker {
-        public class Sigma{
-            public ADecl[] arguments;
-            public Type returnType;
+    public static enum TypeCode { CInt, CDouble, CBool, CVoid } ;
+    public static class Sigma{
+        public LinkedList<TypeCode> arguments;
+        public TypeCode returnType;
 
-            public Sigma(ListArg args, Type type){
-                arguments = new ADecl[args.size()];
-                for (int i =0; i<args.size(); i++){
-                    arguments[i]= (ADecl)args.get(i);
-                }
-                returnType=type;
-            }
+    }
+    public static class Env {
+        public HashMap<String,Sigma> signature ;
+        public LinkedList<HashMap<String,TypeCode>> contexts ;
+        public static Type lookupVar(String id) {} ;
+        public static TypeCode lookupFun(String id) {} ;
+        public static void updateVar (String id, Type ty) {} ;
+        // ...
+    }
+        
+    public HashMap<String,Sigma> symbolTable = new HashMap<String,Sigma>();
 
-        }
-        public HashMap<String,Sigma> symbolTable = new HashMap<String,Sigma>();
-
-        public void typecheck(Program p) {
-        PrettyPrinter pp = new PrettyPrinter();
+    public void typecheck(Program p) {
+        //PrettyPrinter pp = new PrettyPrinter();
         PDefs defs = (PDefs)p;
         ListDef listOfDefs = defs.listdef_;
 
         //Add built-in fnctions
-        Sigma printInt = new Sigma(new ListArg(), new Type_void());
+        Sigma printInt = new Sigma();
+        printInt.arguments = new LinkedList<TypeCode>();
+        printInt.returnType = TypeCode.CVoid;
 
+        Sigma printDbl = new Sigma();
+        printDbl.arguments = new LinkedList<TypeCode>();
+        printDbl.returnType = TypeCode.CVoid;
+        
+        Sigma readInt = new Sigma();
+        readInt.arguments = new LinkedList<TypeCode>();
+        readInt.returnType = TypeCode.CInt;
+        
+        Sigma readDbl = new Sigma();
+        readDbl.arguments = new LinkedList<TypeCode>();
+        readDbl.returnType = TypeCode.CDouble;
+        
         symbolTable.put("printInt", printInt);
+        symbolTable.put("printDouble", printDbl);
+        symbolTable.put("readInt", readInt);
+        symbolTable.put("readDouble", readDbl);
+        
+//        for (Def def : listOfDefs) {
+//            DFun function = (DFun)def;
+//            symbolTable.put(function.id_, new Sigma(function.listarg_, function.type_));
+//            CheckFuncion(function);
 
-        for (Def def : listOfDefs) {
-            DFun function = (DFun)def;
-            symbolTable.put(function.id_, new Sigma(function.listarg_, function.type_));
-            CheckFuncion(function);
+//        }
+    System.out.println(symbolTable);
+    throw new TypeException("Not yet a typechecker");
 
+
+
+    }
+      private void checkStm (Stm st , Env ev)
+    {
+        st.accept(new CheckStm() , ev);
+    }
+    
+    public class CheckStm implements Stm.Visitor<Env,Env>{
+        public Env visit(SDecls p, Env env) {
+        
         }
-        System.out.println(symbolTable);
-        throw new TypeException("Not yet a typechecker");
-
-
-
+        public Env visit(SExp p, Env env) {
+        
+        }
     }
-    public void CheckFuncion(DFun fun){
-        //Lägg till argument i var-lista
-
-
-        //Gå alla statements och type-checka
-    }
-
 }
