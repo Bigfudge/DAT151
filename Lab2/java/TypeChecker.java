252404b194278ba743f7264cabb9905281c3b00f
Simon Persson <simonpersson@flaskpost.org>
2018-11-26 11:57:01 +0100

    Added first symboltable

--- Lab2/java/TypeChecker.java
+++ Lab2/java/TypeChecker.java
@@ -6,34 +6,21 @@ import java.util.Set;
 
 public class TypeChecker {
         public class Sigma{
-            private AType[] arguments;
-            private AType returnType;
-            public AType[] getArgs(){
-                return arguments;
-            }
-            public AType getReturnType(){
-                return returnType;
-            }
+            public ADecl[] arguments;
+            public Type returnType;
+
             public Sigma(ListArg args, Type type){
-                arguments = new AType[args.length];
-                for (int i =0; i<args.length; i++){
-                    arguments[i] = setType(args[i]);
+                arguments = new ADecl[args.size()];
+                for (int i =0; i<args.size(); i++){
+                    arguments[i]= (ADecl)args.get(i);
                 }
-                returnType=setType(type);
+                returnType=type;
             }
 
         }
         public enum AType{INT,DOUBLE, BOOL, VOID; };
         public HashMap<String,Sigma> symbolTable = new HashMap<String,Sigma>();
 
-        public AType setType(Type type){
-            if (type instanceof Type_int)  return AType.INT;
-            else if(type instanceof Type_double) return AType.DOUBLE;
-            else if(type instanceof Type_bool) return AType.BOOL;
-            else if(type instanceof Type_void) return AType.VOID;
-            else throw new TypeException("Not yet a typechecker");
-
-            }
 
 
         public void typecheck(Program p) {
@@ -45,6 +32,8 @@ public class TypeChecker {
             DFun function = (DFun)def;
 
             symbolTable.put(function.id_, new Sigma(function.listarg_, function.type_));
+            System.out.println(symbolTable);
+            System.out.println(symbolTable.get("main"));
 
         }
 
